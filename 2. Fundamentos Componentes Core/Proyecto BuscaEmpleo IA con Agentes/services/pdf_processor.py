e05414847a40d077beeaac31424efc26ea18a7b5
Mariano SaSo <152398164+Cuco89@users.noreply.github.com>
2026-06-10 10:46:58 +0200

    cambio en la funcion de extraccion de texto pdf

--- 2. Fundamentos Componentes Core/Proyecto BuscaEmpleo IA con Agentes/services/pdf_processor.py
+++ 2. Fundamentos Componentes Core/Proyecto BuscaEmpleo IA con Agentes/services/pdf_processor.py
@@ -1,8 +1,9 @@
 import PyPDF2 # Para leer PDF, que esta en bytes. Por ello tenemos que extraer el texto y procesarlo
 from io import BytesIO #Es un modulo para leer esos bytes.
 
+# lee un PDF → extrae el texto → lo devuelve como string
 def extraer_texto_pdf(archivo_pdf):
-    """Extrae el texto del CV del usuario"""
+    """Primero lee el texto y despues Extrae el texto del CV del usuario"""
     try:
         archivo_pdf.seek(0)
         pdf_reader = PyPDF2.PdfReader(BytesIO(archivo_pdf.read()))
@@ -13,3 +14,8 @@ def extraer_texto_pdf(archivo_pdf):
         return texto_completo.strip()
     except Exception as e:
         return f"Error leyendo PDF: {e}"
+
+#0. archivo_pdf.seek(0) hace que el cursor de lectura del archivo pdf vaya siempre al inicio
+#1. archivo_pdf.read() 👉 lee el archivo en bytes
+#2. BytesIO(...) 👉 convierte bytes en “archivo virtual”
+#3. PdfReader(...) 👉 PyPDF2 abre el PDF y lo interpreta
